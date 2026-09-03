ace43b6554451591d84780e4be293477c6ebde16
katttiger <katttiger@gmail.com>
2026-09-03 14:30:51 +0200

    added flex and rowbreak to header with tailwindcss

--- task-board/src/components/Header.tsx
+++ task-board/src/components/Header.tsx
@@ -1,10 +1,9 @@
 const Header = () => {
   return (
-    <header className="text-blue-100 bg-slate-500 py-10 flex justify-center">
-      <h2 className="px-3">Team Task Board</h2>
-      <br />
-      <h3>Huvudrubrik</h3>
-      <p>En kort beskrivning</p>
+    <header className="bg-slate-500 text-white py-10 flex flex-col items-center text-center">
+      <h2 className="text-3xl font-bold mb-2">Team Task Board</h2>
+      <h3 className="text-xl text-slate-300 font-emdium mb-1">Huvudrubrik</h3>
+      <p className="text-sm">En kort beskrivning</p>
     </header>
   );
 };
