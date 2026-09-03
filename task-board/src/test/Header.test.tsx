f665b313878eed339a89cdfec11a06aecfe65c90
katttiger <katttiger@gmail.com>
2026-09-03 10:38:46 +0200

    added tests for header.tsx

---
+++ task-board/src/test/Header.test.tsx
@@ -0,0 +1,26 @@
+//Describe för att grupera testerna
+import { describe, it, expect } from "vitest";
+import Header from "../components/Header";
+import { screen, render } from "@testing-library/react";
+
+describe("Rendering", () => {
+  it("Header rendered without error", () => {
+    render(<Header />);
+    expect(
+      screen.getByRole("heading", { name: "Team Task Board" }),
+    ).toBeInTheDocument();
+  });
+});
+
+//Visa rubrik
+describe("Text in header rendered without error", () => {
+  it("Header is displayed", () => {
+    render(<Header />);
+    expect(screen.getByText("Huvudrubrik")).toBeInTheDocument();
+  });
+
+  it("description is displayed", () => {
+    render(<Header />);
+    expect(screen.getByText("En kort beskrivning")).toBeInTheDocument();
+  });
+});
