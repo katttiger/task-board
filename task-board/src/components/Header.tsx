aff140dbc5323f46925715e89111ff94fd3cf91c
katttiger <katttiger@gmail.com>
2026-09-06 16:28:51 +0200

    added form to create a new task. Added CSS to form. Ensured responsitivity with mobile devices. Adjusted header css.

--- task-board/src/components/Header.tsx
+++ task-board/src/components/Header.tsx
@@ -1,9 +1,15 @@
 const Header = () => {
   return (
-    <header className="bg-slate-500 text-white py-10 flex flex-col items-center text-center">
-      <h2 className="text-3xl font-bold mb-2">Team Task Board</h2>
-      <h3 className="text-xl text-slate-300 font-medium mb-1">Team Java</h3>
-      <p className="text-sm">Anslagstavla för uppgifter för Team Java</p>
+    <header className="bg-slate-500 text-white py-10 flex flex-col items-center text-center font-mono">
+      <h1 className="text-3xl rotate-4 z-1 font-bold uppercase bg-slate-900 p-2">
+        Team Task Board
+      </h1>
+      <h2 className="rotate-3 text-xl text-white font-medium uppercase bg-slate-700 -mt-2 p-3">
+        Team Java
+      </h2>
+      <h3 className="-rotate-12 -mt-2 mb-5 z-1 border-t-2 border-slate-700 text-sm uppercase p-4 bg-slate-900">
+        Anslagstavla för uppgifter för Team Java
+      </h3>
     </header>
   );
 };
--- task-board/src/components/NewTaskForm.tsx
+++ task-board/src/components/NewTaskForm.tsx
@@ -23,8 +23,8 @@ const NewTaskForm = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("Låg");
-  const [assignee, setAssignee] = useState(assignees[0]);
-  const [category, setCategory] = useState(categories[0]);
+  const [assignee, setAssignee] = useState(assignees[0].value);
+  const [category, setCategory] = useState(categories[0].value);
 
   const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -36,25 +36,33 @@ const NewTaskForm = () => {
   };
 
   return (
-    <div className="w-full max-w-xs flex justify-center">
+    <div className="w-auto flex justify-center m-6">
       <form
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4"
+        className="bg-amber-200 border-3 shadow-2xl border-orange-300 shadow rounded px-8 pt-6 pb-8 mb-4 mt-4 s:w-full"
         onSubmit={handleSubmit}
       >
-        <div>
-          <label id="title">Uppgift</label>
+        <div className="p-3">
+          <label htmlFor="title" className="bg-orange-100 p-4">
+            Uppgift
+          </label>
           <input
-            className="my-2"
+            name="title"
+            className="focus:bg-orange-200 my-2 border-2 border-orange-300 p-4 ml-0 bg-white w-full"
             value={title}
             onChange={(event) => {
               setTitle(event.target.value);
             }}
-            placeholder="Uppgift"
+            placeholder="Uppgiftens namn"
           />
         </div>
-        <div>
-          <label id="priority">Prioritet</label>
+
+        <div className="mb-5 p-3 lg:hidden">
+          <label htmlFor="priority" className="bg-orange-100 p-4">
+            Prioritet
+          </label>
           <select
+            id="priority"
+            className="p-4 mt-2 focus:bg-orange-100 bg-white focus:bg-orange-200 border border-black-100 w-full"
             value={priority}
             onChange={(event) => setPriority(event.target.value)}
           >
@@ -63,9 +71,17 @@ const NewTaskForm = () => {
             <option>Hög</option>
           </select>
         </div>
-        <div>
-          <label id="category">Kategori</label>
-          <select onChange={(event) => setCategory(event.target.value)}>
+
+        <div className="mb-5 p-3 lg:hidden">
+          <label htmlFor="category" className="bg-orange-100 p-4">
+            Kategori
+          </label>
+          <select
+            id="category"
+            className="p-4 focus:bg-orange-100 bg-white focus:bg-orange-200 border border-black-100 w-full mt-2"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          >
             {categories.map((category) => (
               <option key={category.key} value={category.value}>
                 {category.value}
@@ -73,9 +89,63 @@ const NewTaskForm = () => {
             ))}
           </select>
         </div>
-        <div>
-          <label id="assignee">Ansvarig</label>
-          <select onChange={(event) => setAssignee(event.target.value)}>
+
+        <div className="mb-5 p-3 hidden lg:flex">
+          <label htmlFor="priority" className="bg-orange-100 p-4">
+            Prioritet
+          </label>
+          <select
+            id="priority"
+            className="p-4 focus:bg-orange-100 bg-white focus:bg-orange-200 border border-black-100"
+            value={priority}
+            onChange={(event) => setPriority(event.target.value)}
+          >
+            <option>Låg</option>
+            <option>Medel</option>
+            <option>Hög</option>
+          </select>
+
+          <label htmlFor="category" className="ml-2 bg-orange-100 p-4">
+            Kategori
+          </label>
+          <select
+            id="category"
+            className="p-4 bg-white border-1 focus:bg-orange-200"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          >
+            {categories.map((category) => (
+              <option key={category.key} value={category.value}>
+                {category.value}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="p-3">
+          <label htmlFor="description" className="mb-20 bg-orange-100 p-4">
+            Beskrivning
+          </label>
+          <br></br>
+          <textarea
+            name="description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            placeholder="Uppgiftsbeskrivning"
+            className="focus:bg-orange-200 p-4 border-2 border-orange-300 text-left mt-3 bg-white w-full h-50"
+          ></textarea>
+        </div>
+
+        <div className="p-3">
+          <label htmlFor="assignee" className="bg-orange-100 p-4">
+            Ansvarig
+          </label>
+          <select
+            id="assignee"
+            value={assignee}
+            onChange={(event) => setAssignee(event.target.value)}
+            className="p-4 w-auto bg-white border-1 focus:bg-orange-200"
+          >
             {assignees.map((person) => (
               <option key={person.key} value={person.value}>
                 {person.value}
@@ -83,15 +153,12 @@ const NewTaskForm = () => {
             ))}
           </select>
         </div>
-
-        <label id="description">Beskrivning</label>
-        <textarea
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          placeholder="Uppgiftsbeskrivning"
-        ></textarea>
-
-        <button type="submit">Skapa uppgift</button>
+        <button
+          className="ml-3 border-orange-300 bg-orange-100 hover:bg-orange-300 active:bg-orange-500 border-2 rounded px-4 py-2"
+          type="submit"
+        >
+          Skapa uppgift
+        </button>
       </form>
     </div>
   );
